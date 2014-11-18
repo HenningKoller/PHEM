0df7f1354eb7ef6849295016eb2309d7f4170ed6
Henning Koller <henning.gkoller@gmail.com>
2014-11-18 20:05:34 +0100

    Deleted old structure

--- app/app.js
+++
@@ -1,8 +0,0 @@
-angular.module('myApp', [
-    'ngRoute',
-    'myApp.index',
-    'myApp.form'
-]).
-    config(['$routeProvider', function($routeProvider) {
-        $routeProvider.otherwise({redirectTo: '/index'});
-    }]);
