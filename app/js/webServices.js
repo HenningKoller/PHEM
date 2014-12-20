f5cdb1bab4bca9089f61432e92e61beec4b67ecb
emilfo <skalkanskje@gmail.com>
2014-12-20 13:23:10 +0100

    Removed program selctor, now fetching program by name QA-Tool

--- app/js/webServices.js
+++ app/js/webServices.js
@@ -13,6 +13,22 @@ appServices.factory('apiServices', function ($resource, $rootScope, $q, $http) {
     $rootScope.rootUrl = serverResponse.activities.dhis.href;
 
     return {
+        getProgramByName : function(name){
+            var deffered = $q.defer();
+            $http.get($rootScope.rootUrl + '/api/programs.json')
+                .success(function(data){
+                    for(var i = 0; i < data.programs.length; i++){
+                        if(data.programs[i].name === name){
+                            deffered.resolve(data.programs[i]);
+                        }
+                    }
+                    deffered.reject("Unknown Unit");
+                })
+                .error(function(data, status, headers){
+                    deffered.reject("Error: " + status);
+                });
+            return deffered.promise;
+        },
         getProgram: function () {
             return $resource($rootScope.rootUrl + '/api/programs.json', {},
                 {
